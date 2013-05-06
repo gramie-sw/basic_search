a3ecde7b6e905245d47e9a2efc8ec114ad781a19
cmierich <cm.gramie@gmail.com>
2013-05-06 20:35:23 +0200

    BasicSearch::Ralitie fixed

--- lib/basic_search/railtie.rb
+++ lib/basic_search/railtie.rb
@@ -1,9 +1,9 @@
 module BasicSearch
   class Railtie < Rails::Railtie
-    initializer "basic_table_sorter.controller_additions" do
-      ActionModel::Base.send :include, ControllerAdditions
+    initializer "basic_search.active_record_additions" do
+      ActionRecord::Base.send :include, ActiveRecordAdditions
     end
-    initializer "basic_table_sorter.view_helpers" do
+    initializer "basic_search.view_helpers" do
       ActionView::Base.send :include, ViewHelpers
     end
   end
