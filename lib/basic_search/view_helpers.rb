87aaa7e81e34bd19da80df7360b33c687939c082
cmierich <cm.gramie@gmail.com>
2013-05-06 22:01:53 +0200

    ViewHelper implemented

--- lib/basic_search/view_helpers.rb
+++ lib/basic_search/view_helpers.rb
@@ -1,5 +1,12 @@
 module BasicSearch
   module ViewHelpers
 
+    def params_search_hash
+      params[:search] ? { search: params[:search] } : {}
+    end
+
+    def search_param_hash columns, title
+      sortable(columns, title, params_search_hash)
+    end
   end
 end
---
+++ spec/basic_search/view_helper_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'ViewHelpers' do
+
+  before :each do
+    @view_helper_class = Class.new
+    @view_helper_class.send(:include, BasicSearch::ViewHelpers)
+    @view_helper = @view_helper_class.new
+  end
+
+  describe '#search_params_hash' do
+
+    it 'should return existing hash with key search from params' do
+      @view_helper.stub(:params).and_return({search: 'search_value'})
+      @view_helper.params_search_hash.should eq({search: 'search_value'})
+    end
+
+    it 'should return empty hash if params has no hash with key search' do
+      @view_helper.stub(:params).and_return({})
+      @view_helper.params_search_hash.should eq({})
+    end
+  end
+
+  describe '#search_param_hash' do
+
+    it 'should call sortable' do
+      @view_helper.should_receive(:params_search_hash).and_return({search: 'search_value'})
+      @view_helper.should_receive(:sortable).with(['column1'], 'title', {search: 'search_value'})
+      @view_helper.search_param_hash(['column1'], 'title')
+    end
+  end
+end
